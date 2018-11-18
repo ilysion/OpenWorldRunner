894990b08d0108b1a28d10bf83de058be8b9d6c5
reneekroon <36478130+reneekroon@users.noreply.github.com>
2018-11-18 03:17:50 +0200

    Moving along terrain (and jump)
    jumping is broken, it doesn't reset yet (you can only jump once and it is stuck in jumping mode after that)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
+	public float jumpHeight;
+	public Terrain terrain;
+	// Distance from center of model to bottom
+	private float playerHeightOffset = 1;
+	Rigidbody rb; 
+	bool jumping = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -16,17 +22,37 @@ public class PlayerController : MonoBehaviour {
 		
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
+		float jump = Input.GetAxis("Jump");
 
-		Vector3 direction;
+		if (jump > 0 && !jumping) {
 
-		// Makes diagonal movement as fast as moving in a straight line
+			rb.isKinematic = false;
+			rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
+			jumping = true;
+		}
+
+ 		Vector3 direction;
+
+ 		// Makes diagonal movement as fast as moving in a straight line
 		if (Mathf.Abs(horizontal) + Mathf.Abs(vertical) > 1) {
 			direction = Time.deltaTime * Vector3.Normalize(new Vector3(horizontal, 0, vertical));
 		} else {
-			direction = new Vector3(horizontal * Time.deltaTime, 0, vertical * Time.deltaTime);
+			direction = Time.deltaTime * new Vector3(horizontal, 0, vertical);
 		}
 
-		transform.Translate(moveSpeed * direction);
+		float deltaHeight = terrain.SampleHeight(transform.position) - transform.position.y + playerHeightOffset;
+		Debug.Log(deltaHeight);
 
-	}
+		if (!jumping) {
+ 			transform.Translate(moveSpeed * direction.x, deltaHeight, moveSpeed * direction.z);
+		} else {
+			// Moving while jumping can be configured here
+			transform.Translate(moveSpeed * direction.x, 0, moveSpeed * direction.z);
+
+			//if (deltaHeight < 0.5) {
+			//	rb.isKinematic = true;
+			//	jumping = false;
+			//}
+		}
+ 	}
 }
