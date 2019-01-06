61a87b85e80ca98277420d126e9c363e3e834ba1
reneekroon <36478130+reneekroon@users.noreply.github.com>
2019-01-06 15:54:07 +0200

    Added Bike
    And camera now stays above terrain

---
+++ Assets/Scripts/BikeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeController : MonoBehaviour {
+
+	public float acceleration = 400f;
+	public float turning = 200f; 
+	public float dragOnGround = 2f;
+	private Rigidbody rb; 
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody>();
+		//Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float horizontal = Input.GetAxis("Horizontal"); //TODO Time.Deltatime
+		float vertical = Input.GetAxis("Vertical");
+		
+		// Distance to terrain below bike 
+		RaycastHit hit;
+		Ray downRay = new Ray(transform.position, Vector3.down);
+		if (Physics.Raycast(downRay, out hit)) {
+			Debug.Log(hit.distance);
+			// ???
+		}
+
+
+		// Bike on ground - enable controlling the bike
+		if (hit.distance <= 0.5) {
+			
+			rb.drag = dragOnGround;
+
+			// Acceleration 
+			rb.AddForce(transform.forward * vertical * acceleration);
+
+			// Turning 
+			rb.AddTorque(transform.up * horizontal * turning);
+
+		// Bike in air
+		} else {
+
+			rb.drag = 0.1f;
+
+		}
+
+		// Keeps the bike upright
+		if (horizontal == 0 || hit.distance > 0.5) {
+			rb.AddTorque(transform.forward * (transform.rotation.z * -50));
+		}
+
+		//TODO do something when the bike falls over (rotate back up)
+
+ 	}
+
+}
--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -23,12 +23,34 @@ public class MainCamera : MonoBehaviour {
 
 		// Stick to player
 		Vector3 playerPosition = player.transform.position;
-		Vector3 cameraPositionForPlayer = new Vector3(playerPosition.x, playerPosition.y + height, playerPosition.z);
+		Vector3 cameraPositionForPlayer = new Vector3(playerPosition.x, playerPosition.y + height, playerPosition.z - distance);
+
+		// Find distance to ground
+		RaycastHit hit;
+		Ray downRay = new Ray(cameraPositionForPlayer, Vector3.down);
+		if (Physics.Raycast(downRay, out hit)) {
+			Debug.Log("Camera: " + hit.distance);
+			// ???
+		} else {
+			// If camera would appear under terrain cast ray upwards
+			Ray upRay = new Ray(cameraPositionForPlayer, Vector3.up);
+			if (Physics.Raycast(upRay, out hit)) {
+				Debug.Log("Camera Below: " + hit.distance);
+				// ???
+			}
+		}
+			
+		// Make camera stay above ground
+		if (hit.distance < 5) {
+			cameraPositionForPlayer.y = hit.point.y + 5;
+		}
 
 		// Rotate around player depending on player's rotation
-		Vector3 cameraPositionForRotation = distance * new Vector3(-1 * Mathf.Sin(playerAngle), 0, -1 * Mathf.Cos(playerAngle));
+		//Vector3 cameraPositionForRotation = distance * new Vector3(-1 * Mathf.Sin(playerAngle), 0, -1 * Mathf.Cos(playerAngle));
 
-		transform.position = cameraPositionForPlayer + cameraPositionForRotation;
+		//Vector3 cameraPositionForRotation = distance * new Vector3(-1, 0, -1);
+		
+		transform.position = cameraPositionForPlayer; // + cameraPositionForRotation;
 		transform.LookAt(playerPosition);
 
 	}
